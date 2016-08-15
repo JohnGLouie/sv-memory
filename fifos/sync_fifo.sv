367bb69f337ac88101ef4f30a602638b3855a582
johngilouie <johngilouie@gmail.com>
2016-08-14 17:20:38 -0700

    Fifo Added
    I think I did this right...

--- fifos/sync_fifo.sv
+++ fifos/sync_fifo.sv
@@ -18,6 +18,7 @@ module sync_fifo #(
     )
     (
         input clk_i,
+        input rst,
 
         input we_i,
         input [DATA_WIDTH-1:0] data_i,
@@ -33,7 +34,55 @@ module sync_fifo #(
     )
 
     localparam int ADDR_WIDTH = $clog2(FIFO_DEPTH);     // Address width for RAM.
+    													// Personal Note for John: $clog is built in function, Log base 2 of (x)
 
     // TODO: JOHN, use the ram.sv module for the memory for the fifo.
+
+	logic [ADDR_WIDTH - 1:0] w_pointer;
+	logic [ADDR_WIDTH - 1:0] r_pointer;
+	logic [DATA_WIDTH - 1:0] ram_data; 
+
+    assign full_o = (count_o == (ADDR_WIDTH - 1));
+    assign empty_o = (count_o == 0);
+
+    always_ff@(posedge clk_i)
+    	begin : Write_Pointer
+    	if (rst) begin
+            w_pointer <= 0;
+        end else if (we_i) begin
+    		w_pointer <= w_pointer + 1;
+    	end
+    end
+
+    always_ff@(posedge clk_i)
+    	begin : Read_Pointer
+    	if (rst) begin
+            data_o <= 0;
+            r_pointer <= 0;
+        end else if (re_i) begin
+    		data_o <= ram_data;
+    		r_pointer <= r_pointer + 1;
+    	end
+    end
+
+    always_ff @ (posedge clk_i)
+    	begin : Status_Counter
+    	//Read but no write
+    	if (rst) begin
+            count_o = 0;
+        end else if ((re_i) &&  !(we_i) && (count_o !=0)) begin
+    		count_o <= count_o - 1;
+    	end
+    	else if ((we_i) && (re_i) && (count_o)) begin
+    		count_o <= count_o + 1;
+    	end
+    end
+
+    ram #(ADDR_WIDTH, DATA_WIDTH) ram(
+        .addr_a_i           ((w_pointer & we_i) | (r_pointer & re_i)),
+        .data_rd_a_reg_o    (ram_data),
+        .data_rd_a_o        (ram_data)
+	);
+
 
 endmodule
